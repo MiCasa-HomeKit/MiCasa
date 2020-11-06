10593ff80e56503e3df121da0cfcf18963a0b69b
Thomas Bonk <thomas@meandmymac.de>
2020-11-06 11:01:23 +0100

    - optimized plugin loading - better error handling

--- Sources/micasa/main.swift
+++ Sources/micasa/main.swift
@@ -1,12 +1,12 @@
 /*
  Copyright 2020 MiCasa Development Team
-
+ 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,28 +19,28 @@ import ArgumentParser
 import micasaLib
 
 struct MiCasa: ParsableCommand {
-    @Option(name: .shortAndLong, help: "Directories where plugins are stored.")
-    var pluginDirs: [String] = ["/usr/local/lib/mi-casa-plugins"]
-
-    @Option(name: .shortAndLong, help: "Fully qualified path of the cache file.")
-    var storageFile: String = "/var/mi-casa/cache.json"
-
-    @Option(name: .shortAndLong, help: "Fully qualified path of the configuration file.")
-    var configFile: String = "/etc/mi-casa.conf"
-
-    mutating func run() throws {
-        let pluginDirUrls = pluginDirs.map { pluginDir in URL(fileURLWithPath: pluginDir, isDirectory: true) }
-        let configFileUrl = URL(fileURLWithPath: configFile, isDirectory: false)
-        let storageFileUrl = URL(fileURLWithPath: storageFile, isDirectory: false)
-
-        try Bridge
-            .shared
-            .initialize(
-                configFile: configFileUrl,
-                storageFile: storageFileUrl,
-                pluginDirs: pluginDirUrls)
-        try Bridge.shared.start()
-    }
+  @Option(name: .shortAndLong, help: "Directories where plugins are stored.")
+  var pluginDirs: [String] = ["/usr/local/lib/mi-casa-plugins"]
+  
+  @Option(name: .shortAndLong, help: "Fully qualified path of the cache file.")
+  var storageFile: String = "/var/mi-casa/cache.json"
+  
+  @Option(name: .shortAndLong, help: "Fully qualified path of the configuration file.")
+  var configFile: String = "/etc/mi-casa.conf"
+  
+  mutating func run() throws {
+    let pluginDirUrls = pluginDirs.map { pluginDir in URL(fileURLWithPath: pluginDir, isDirectory: true) }
+    let configFileUrl = URL(fileURLWithPath: configFile, isDirectory: false)
+    let storageFileUrl = URL(fileURLWithPath: storageFile, isDirectory: false)
+    
+    try Bridge
+      .shared
+      .initialize(
+        configFile: configFileUrl,
+        storageFile: storageFileUrl,
+        pluginDirs: pluginDirUrls)
+    try Bridge.shared.start()
+  }
 }
 
 MiCasa.main()
--- Sources/micasaLib/Bridge.swift
+++ Sources/micasaLib/Bridge.swift
@@ -1,12 +1,12 @@
 /*
  Copyright 2020 MiCasa Development Team
-
+ 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,189 +21,246 @@ import MiCasaPlugin
 import AnyCodable
 
 
-public final class Bridge {    
+public final class Bridge {
+  // MARK: - Public Class Properties
+  
+  public private(set) static var shared: Bridge = { Bridge() }()
+  
+  
+  // MARK: - Public Properties
+  
+  public let logger = Logger(label: "mi-casa.bridge")
+  
+  public private(set) var accessoryMap: [Accessory: MiCasaPlugin] = [:]
+  public private(set) var plugins: [MiCasaPlugin]!
+  public private(set) var loggers: [MiCasaPlugin: Logger] = [:]
+  
+  
+  // MARK: - Private Properties
 
-    // MARK: - Public Class Properties
+  private  var configFileUrl: URL!
+  private  var storageFileUrl: URL!
+  private  var pluginDirUrls: [URL]!
+  private  var keepRunning = true
+  private  var configuration: Configuration!
+  private  var pluginManager: PluginManager<MiCasaPlugin>!
+  internal var bridge: Device!
+  private  var bridgeDelegate: BridgeDelegate!
+  private  var server: Server! = nil
+  
+  
+  // MARK: - Initialization
+  
+  private init() {
+    // Empty by design
+  }
+  
+  public func initialize(
+    configFile configFileUrl: URL,
+    storageFile storageFileUrl: URL,
+    pluginDirs pluginDirUrls: [URL]) throws {
+    self.configFileUrl = configFileUrl
+    self.storageFileUrl = storageFileUrl
+    self.pluginDirUrls = pluginDirUrls
 
-    public private(set) static var shared: Bridge = { Bridge() }()
+    logger.info("Initializing MiCasa Bridge")
 
-
-    // MARK: - Public Properties
-
-    public let logger = Logger(label: "mi-casa.bridge")
-
-    public private(set) var accessoryMap: [Accessory:MiCasaPlugin] = [:]
-    public private(set) var plugins: [MiCasaPlugin]!
-    public private(set) var loggers: [MiCasaPlugin:Logger] = [:]
-
-
-    // MARK: - Private Properties
-
-    private  var keepRunning = true
-    private  var configuration: Configuration!
-    private  var pluginManager: PluginManager<MiCasaPlugin>!
-    internal var bridge: Device!
-    private  var bridgeDelegate: BridgeDelegate!
-    private  var server: Server! = nil
+    configuration = try Configuration.load(from: configFileUrl)
+    pluginManager = PluginManager(configuration.plugins.map{ $0.plugin }, withUrls: pluginDirUrls)
+    bridge =
+      Device(
+        bridgeInfo:
+          Service.Info(
+            name: configuration.bridge.name,
+            serialNumber: configuration.bridge.serialNumber,
+            manufacturer: "MiCasa Development Team",
+            model: "MiCasa Bridge",
+            firmwareRevision: "0.0.1"),
+        setupCode: Device.SetupCode(stringLiteral: configuration.bridge.setupCode),
+        storage: FileStorage(filename: storageFileUrl.path),
+        accessories: [])
+    bridgeDelegate = BridgeDelegate(bridge: bridge)
+    bridge.delegate = bridgeDelegate
     
-
-    // MARK: - Initialization
-
-    private init() {
-        // Empty by design
-    }
-
-    public func initialize(
-        configFile configFileUrl: URL,
-        storageFile storageFileUrl: URL,
-        pluginDirs pluginDirUrls: [URL]) throws {
+    let pluginBuilders = pluginBuildersFor(plugins: configuration.plugins)
+    
+    plugins = build(plugins: configuration.plugins, with: pluginBuilders)
+    createLoggers(for: plugins)
+    bridge
+      .addAccessories(
+        initializeAccessories(from: plugins))
+  }
+  
+  private func initializeAccessories(from plugins: [MiCasaPlugin]) -> [Accessory] {
+    let pluginAccessories =
+      plugins
+      .map { plugin in
+        (plugin: plugin, accessories: try! plugin.accessories())
+      }
+    
+    pluginAccessories
+      .forEach { pluginAccessory in
+        pluginAccessory
+          .accessories
+          .forEach { accessory in
+            accessoryMap[accessory] = pluginAccessory.plugin
+          }
+      }
+    
+    return
+      pluginAccessories
+      .map { pluginAccessory in
+        pluginAccessory.accessories
+      }
+      .flatMap { $0 }
+  }
+  
+  private func pluginBuildersFor(plugins: [PluginConfiguration]) -> [MiCasaPluginBuilder<MiCasaPlugin>] {
+    return
+      plugins
+      .map { pluginConfiguration in
+        pluginConfiguration.plugin
+      }
+      .map { pluginName in
+        pluginManager.pluginBuilder(forPlugin: pluginName)
+      }
+      .filter { pluginBuilder in
+        pluginBuilder != nil
+      }
+      .map { pluginBuilder in
+        pluginBuilder!
+      }
+  }
+  
+  private func build(
+    plugins: [PluginConfiguration],
+    with builders: [MiCasaPluginBuilder<MiCasaPlugin>]) -> [MiCasaPlugin] {
+    return
+      builders
+      .map { builder in
+        let config = configuration(for: builder.pluginName, from: plugins)
+        let jsonData = try! JSONEncoder().encode(config)
         
-        configuration = try Configuration.load(from: configFileUrl)
-        pluginManager = try PluginManager(withUrls: pluginDirUrls)
-        bridge =
-            Device(
-                bridgeInfo:
-                    Service.Info(
-                        name: configuration.bridge.name,
-                        serialNumber: configuration.bridge.serialNumber,
-                        manufacturer: "MiCasa Development Team",
-                        model: "MiCasa Bridge",
-                        firmwareRevision: "0.0.1"),
-                setupCode: Device.SetupCode(stringLiteral: configuration.bridge.setupCode),
-                storage: FileStorage(filename: storageFileUrl.path),
-                accessories: [])
-        bridgeDelegate = BridgeDelegate(bridge: bridge)
-        bridge.delegate = bridgeDelegate
+        return try! builder.build(apiGateway: self, configuration: jsonData)
+      }
+  }
+  
+  private func configuration(for pluginName: String, from configurations: [PluginConfiguration]) -> [String: AnyCodable] {
+    return
+      configurations
+      .first { pluginConf in
+        pluginConf.plugin == pluginName
+      }
+      .map { pluginConf in
+        pluginConf.configuration
+      }!
+  }
+  
+  private func createLoggers(for plugins: [MiCasaPlugin]) {
+    plugins
+      .forEach { plugin in
+        loggers[plugin] = Logger(label: String(describing: plugin.self))
+      }
+  }
+  
+  
+  // MARK: - API
 
-        let pluginBuilders = pluginBuildersFor(plugins: configuration.plugins)
+  public func start() throws {
+    logger.info("Starting MiCasa Bridge")
 
-        plugins = build(plugins: configuration.plugins, with: pluginBuilders)
-        createLoggers(for: plugins)
-        bridge
-            .addAccessories(
-                initializeAccessories(from: plugins))
+    keepRunning = true
 
-        plugins.forEach { plugin in
-            plugin.start()
+    signal(SIGINT) { _ in
+      Bridge.shared.stop()
+    }
+    signal(SIGTERM) { _ in
+      Bridge.shared.stop()
+    }
+
+    server = try Server(device: bridge, listenPort: 8000)
+
+    plugins.forEach { plugin in
+      DispatchQueue.main.async {
+        do {
+          try plugin.start()
+        } catch {
+          self.logger.error("Error while starting plugin '\(plugin)': \(error)")
         }
+      }
     }
 
-    private func initializeAccessories(from plugins: [MiCasaPlugin]) -> [Accessory] {
-        let pluginAccessories =
-            plugins
-                .map { plugin in
-                    (plugin: plugin, accessories: plugin.accessories())
-                }
-
-        pluginAccessories
-            .forEach { pluginAccessory in
-                pluginAccessory
-                    .accessories
-                    .forEach { accessory in
-                        accessoryMap[accessory] = pluginAccessory.plugin
-                    }
-            }
-
-        return
-            pluginAccessories
-                .map { pluginAccessory in
-                    pluginAccessory.accessories
-                }
-                .flatMap { $0 }
+    withExtendedLifetime([]) {
+      while keepRunning {
+        RunLoop.current.run(mode: .default, before: Date.distantFuture)
+      }
     }
-
-    private func pluginBuildersFor(plugins: [PluginConfiguration]) -> [MiCasaPluginBuilder<MiCasaPlugin>] {
-        return
-            plugins
-                .map { pluginConfiguration in
-                    pluginConfiguration.plugin
-                }
-                .map { pluginName in
-                    pluginManager.pluginBuilder(forPlugin: pluginName)
-                }
-                .filter { pluginBuilder in
-                    pluginBuilder != nil
-                }
-                .map { pluginBuilder in
-                    pluginBuilder!
-                }
-    }
-
-    private func build(
-        plugins: [PluginConfiguration],
-        with builders: [MiCasaPluginBuilder<MiCasaPlugin>]) -> [MiCasaPlugin] {
-
-        return
-            builders
-                .map { builder in
-                    let config = configuration(for: builder.pluginName, from: plugins)
-                    let jsonData = try? JSONEncoder().encode(config)
-
-                    return builder.build(apiGateway: self, configuration: jsonData!)
-                }
-    }
-
-    private func configuration(for pluginName: String, from configurations: [PluginConfiguration]) -> [String:AnyCodable] {
-        return
-            configurations
-                .first { pluginConf in
-                    pluginConf.plugin == pluginName
-                }
-                .map { pluginConf in
-                    pluginConf.configuration
-                }!
-    }
-
-    private func createLoggers(for plugins: [MiCasaPlugin]) {
-        plugins
-            .forEach { plugin in
-                loggers[plugin] = Logger(label: String(describing: plugin.self))
-            }
-    }
-
-
-    // MARK: - API
-
-    public func stop() {
-        DispatchQueue.main.async {
-            self.logger.info("Shutting MiCasa Bridge down...")
-            self.keepRunning = false
+  }
+  
+  public func stop() {
+    
+    plugins.forEach { plugin in
+      DispatchQueue.main.sync {
+        do {
+          try plugin.stop()
+        } catch {
+          self.logger.error("Error while stopping plugin '\(plugin)': \(error)")
         }
+      }
+    }
+    
+    DispatchQueue.main.async {
+      self.logger.info("Shutting MiCasa Bridge down")
+      self.keepRunning = false
+
+      do {
+        try self.server.stop()
+      } catch {
+        self.logger.error("Error while stopping HAP server: \(error)")
+      }
+    }
+  }
+
+  public func restart() {
+    self.logger.info("Restarting MiCasa Bridge down")
+
+    DispatchQueue.main.async {
+      self.stop()
     }
 
-    public func start() throws {
-        logger.info("Starting MiCasa Bridge")
+    DispatchQueue.main.async {
+      self.accessoryMap = [:]
+      self.plugins = []
+      self.loggers = [:]
 
-        signal(SIGINT) { _ in
-            Bridge.shared.stop()
-        }
-        signal(SIGTERM) { _ in
-            Bridge.shared.stop()
-        }
-
-        server = try Server(device: bridge, listenPort: 8000)
-
-        withExtendedLifetime([]) {
-            while keepRunning {
-                RunLoop.current.run(mode: .default, before: Date.distantFuture)
-            }
-        }
+      do {
+        try self.initialize(
+          configFile: self.configFileUrl,
+          storageFile: self.storageFileUrl,
+          pluginDirs: self.pluginDirUrls)
+      } catch {
+        self.logger.error("Error while initializing the Micasa Bridge: \(error)")
+      }
     }
+
+    DispatchQueue.main.async {
+      do {
+        try self.start()
+      } catch {
+        self.logger.error("Error while restarting MiCasa Bridge: \(error)")
+      }
+    }
+  }
 }
 
 extension Accessory: Hashable {
-
-    // MARK: - Equatable
-
-    public static func == (lhs: Accessory, rhs: Accessory) -> Bool {
-        return lhs.hashValue == rhs.hashValue
-    }
-
-    public var hashValue: Int {
-        return self.serialNumber.hashValue
-    }
-
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(self.serialNumber)
-    }
+  // MARK: - Equatable
+  
+  public static func == (lhs: Accessory, rhs: Accessory) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+  }
+  
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(self.serialNumber)
+  }
 }
